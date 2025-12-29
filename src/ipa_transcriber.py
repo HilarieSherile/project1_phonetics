fe094e91ced5fac8d1033696940c7d37173ab69d
Hilarie Sielenou <HilarieSherile>
2025-12-29 14:44:25 +0300

    UPDATED the main in order to have two different types of phonemic transcriptions: British English and American English. The user can decide at the beginning and always type back in order to switch dialects. The dictionaries for each are different. American english dictionary is a_data_dict.json and the British one can be found in b_data_dict.json. For now, the latter only contains placeholder values as IPA.

--- src/ipa_transcriber.py
+++ src/ipa_transcriber.py
@@ -3,27 +3,63 @@
 import json 
 
 #load IPA dict once 
-with open("utils/data_dict.json", "r", encoding="utf-8") as f: 
-    DATA_DICT = json.load(f)
+with open("utils/a_data_dict.json", "r", encoding="utf-8") as f: 
+    A_DATA_DICT = json.load(f) # to load american english IPA
 
-def get_ipa(word):
-    
+def get_ipa_A(word):
+
+    return A_DATA_DICT.get(word.lower(), "IPA not found")
+
+
+
+with open("utils/b_data_dict.json", "r", encoding="utf-8") as f: 
+    B_DATA_DICT = json.load(f) # to load British english IPA
+
+def get_ipa_B(word):
+    return B_DATA_DICT.get(word.lower(), "IPA not found")
+        
+
     ##Convert a word to its IPA transcription. 
 
-    return DATA_DICT.get(word.lower(), "IPA not found")
 
 def main(): 
     print("Welcome to the IPA Converter!")
     
     while True: 
-        user_input = input ("Enter a word (or type 'exit' to quit): ")
+        user_choice_language = input ("Choose a language (write down the number): \n 1. AMERICAN ENGLISH\n 2. BRITISH ENGLISH \n or type 'exit' to quit. \n")
+        
+        if user_choice_language.lower() == "exit": 
+            print ("Goodbye!")
+            break
 
-        if user_input.lower() == "exit":
-            print("Goodbye!")
-            break 
+        if user_choice_language == "1": 
 
-        ipa = get_ipa(user_input)
-        print(f"IPA transcription: {ipa}\n")
+            while True: 
+                user_input = input ("Enter a word (or type 'back' to quit): ")
+
+                if user_input.lower() == "back":
+                    print("Goodbye!")
+                    break 
+
+                ipa = get_ipa_A(user_input)
+                print(f"IPA transcription: {ipa}\n")
+
+        if user_choice_language == "2": 
+            
+            while True: 
+                user_input = input ("Enter a word (or type 'back' to quit): ")
+                
+                if user_input.lower() == "back": 
+                    print ("Goodbye!")
+                    break 
+                
+                ipa = get_ipa_B(user_input)
+                print(f"IPA transcription: {ipa}\n")
+
+
+
+
+    
 
 
 if __name__ == "__main__": 
